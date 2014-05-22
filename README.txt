2cced6b9d4cfafa36da0b521481c0bb9cd11ab98
audiodev <ljmerza@gmail.com>
2014-05-22 11:31:35 -0400

    first version

---
+++ README.txt
@@ -0,0 +1,2 @@
+I wrote a small program in python that converts an image into css.  It uses the scipy and time libraries.  It asks you the file name (needs to be in the same directory as the python file), the resolution you want, the border-radius, and white space between "pixels".  You can get really high resolution stuff but it crashes my web browser.  
+
---
+++ kinectcss.py
@@ -0,0 +1,70 @@
+from scipy import ndimage
+from time import *
+
+name = raw_input("Enter full name of file (in same directory as this file): ")
+
+res = raw_input("Enter Resolution (recommended >20):")
+res=int(res)
+
+raid = raw_input("Enter border-radius in percent (recommended 0-50): ")
+
+sprd = raw_input("how spread out do you want the pixels? (recommended 1.00-2.00):")
+sprd=float(sprd)
+
+counter1 = 1
+counter2 = 1
+counter3 = 1;
+
+rgb=ndimage.imread(name);
+
+r=rgb[::]
+rlist = r.tolist()
+
+print "width: " + str(len(rlist))
+print "height: " + str(len(rlist[1]))
+print "total pixels in css: " + str(len(rlist)*len(rlist[1])/(res*res))
+var2 = raw_input("are you sure you want to continue? (y/n)")
+
+if(var2=='y'):
+	print "starting..."
+	fcss = open('photo.css', 'w')
+	fhtml = open('photo.html', 'w')
+
+	while(counter1 < len(rlist)):
+		while(counter2 < len(rlist[1])):
+			fcss.write('#circle' + str(counter3) + '{position:absolute;left:' + str(counter2*sprd) +'px;top:' 
+				+ str(counter1*sprd) + 'px;border-radius:' + raid +'%;width:' + str(res) + 'px;height:' + str(res) + 'px;background:rgb(' 
+				+ str(rlist[counter1][counter2][0]) + ',' + str(rlist[counter1][counter2][1]) + ',' + str(rlist[counter1][counter2][2]) + ')}\n')
+			fcss.write(' ')
+			counter3 = counter3+1
+			counter2 = counter2+1
+		counter2=1
+		counter1 = counter1+res
+
+	fhtml.write('<!DOCTYPE html>\n')
+	fhtml.write('<html lang="en">\n')
+	fhtml.write('    <head>\n')
+
+	fhtml.write('        <link rel="stylesheet" href="./photo.css" />\n')
+	fhtml.write('        <title> image to css v0.03 </title>\n')
+	fhtml.write('    </head>\n')
+	fhtml.write('    <body>\n')
+
+	counter1=1
+	counter2=1
+
+	while(counter1 < len(rlist)*len(rlist[1])):
+		fhtml.write("        <div id=\"circle" + str(counter1) + "\"></div>\n")
+		counter1 = counter1 + res
+
+	fhtml.write('    </body>\n')
+	fhtml.write('</html>\n')
+
+	fcss.close()
+	fhtml.close()
+	print "done."
+	sleep(2)
+else:
+	print "aborting."
+	sleep(2)
+
